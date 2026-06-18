f445c65fe154cbc1c04cc1044f02833b6afc94eb
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-18 08:55:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,28 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "node:path";
 import { readFileSync } from "node:fs";
 
-const packageJson = JSON.parse(readFileSync(new URL("./package.json", import.meta.url), "utf8"));
-
+const packageJson = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url), "utf8"),
+);
+const isTauri = process.env.TAURI_PLATFORM !== undefined;
+console.log(process.env.TAURI_PLATFORM)
 export default defineConfig({
-  base: "/app/",
+  base: isTauri ? "./" : "/app/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src")
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: "dist",
+    emptyOutDir: true,
   },
   server: {
     host: "0.0.0.0",
-    port: 4173
+    port: 4173,
   },
   define: {
-    __APP_VERSION__: JSON.stringify(String(packageJson.version || "0.0.0"))
-  }
+    __APP_VERSION__: JSON.stringify(String(packageJson.version || "0.0.0")),
+  },
 });
