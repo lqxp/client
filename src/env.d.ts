ac5cee18f81fe0347fdef20c09af4039ef06a067
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-24 03:01:39 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,9 +8,6 @@ interface ImportMetaEnv {
   readonly VITE_QXP_TURN_URLS?: string;
   readonly VITE_QXP_TURN_USERNAME?: string;
   readonly VITE_QXP_TURN_CREDENTIAL?: string;
-  readonly QXP_TURN_URLS?: string;
-  readonly QXP_TURN_USERNAME?: string;
-  readonly QXP_TURN_CREDENTIAL?: string;
   readonly VITE_QXP_CALLS_ENABLED?: string;
   readonly VITE_QXP_CALLS_UNAVAILABLE_REASON?: string;
 }
