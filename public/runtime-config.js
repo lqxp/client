c6f5ad54f6b18bf6552d4b72639b151a003c8a3d
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-24 02:35:42 +0200

    Update runtime-config.js

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -2,5 +2,8 @@ window.__QXP_RUNTIME__ = {
   ...(window.__QXP_RUNTIME__ || {}),
   serverOrigin: window.__QXP_RUNTIME__?.serverOrigin || "https://qxch.at/app",
   apiBaseUrl: window.__QXP_RUNTIME__?.apiBaseUrl || "https://qxch.at",
-  wsUrl: window.__QXP_RUNTIME__?.wsUrl || "wss://qxch.at/ws"
+  wsUrl: window.__QXP_RUNTIME__?.wsUrl || "wss://qxch.at/ws",
+  rtc: {
+    ...(window.__QXP_RUNTIME__?.rtc || {})
+  }
 };
