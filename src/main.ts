d7067632e29f4831c7fb552a02224223dffbb344
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-29 01:42:44 +0200

    nhew fonts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { initializeRuntimeConfig } from "./config/runtime";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import "./styles.css";
 
 function syncViewportHeight() {
